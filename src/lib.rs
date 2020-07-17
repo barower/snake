d34ab03b0fd6de6dffb57f9ad730219f8b4d6dd9
Bartłomiej Osiński <bartlomiej.osinski@enigma.com.pl>
2020-07-18 01:01:58 +0200

    rust: backend structures literal port

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate libc;
+
+#[repr(C)]
+pub enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
 }
+
+#[repr(C)]
+pub enum Status {
+    SUCCESS,
+    FAILURE,
+}
+
+#[repr(C)]
+pub struct PointList {
+    x: isize,
+    y: isize,
+    next: *mut PointList,
+}
+
+#[repr(C)]
+pub struct Board {
+    snake: *mut PointList,
+    foods: *mut PointList,
+    xmax: isize,
+    ymax: isize,
+}
+
