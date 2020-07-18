f07f59f4c893b2df6a367dbafa89ccdc6591e62c
Bartłomiej Osiński <bartlomiej.osinski@enigma.com.pl>
2020-07-18 22:00:24 +0200

    backend.h: marked functions that are unneeded

--- src/backend.h
+++ src/backend.h
@@ -17,14 +17,19 @@ typedef struct {
   int ymax;
 } Board;
 
-
+// Remove
 extern bool is_same_place(PointList* cell1, PointList* cell2);
 extern enum Status move_snake(Board* board, enum Direction dir);
+// Remove
 extern PointList* next_move(Board* board, enum Direction dir);
+// Remove
 extern PointList* create_cell(int x, int y);
+// Remove
 extern PointList* create_random_cell(int xmax, int ymax);
 extern PointList* create_snake();
 extern Board* create_board(PointList* foods, PointList* snake, int xmax, int ymax);
+// Remove
 extern bool list_contains(PointList* cell, PointList* list);
+// Remove
 extern bool remove_from_list(PointList* elt, PointList** list);
 extern void add_new_food(Board* board);
--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "../src/backend.h"
 
+// Remove
 #test test_create_cell
     PointList* snake = create_cell(2, 3);
     fail_unless(snake->x == 2);
@@ -59,6 +60,7 @@
     fail_unless(board->snake->next->y == 2);
     fail_unless(board->snake->next->next == NULL);
 
+// Remove
 #test test_next_move_corner
     PointList* snake = create_cell(0, 0);
     Board* board = create_board(snake, NULL, 2, 2);
@@ -67,6 +69,7 @@
     fail_unless(next_move(board, DOWN) != NULL);
     fail_unless(next_move(board, RIGHT) != NULL);
 
+// Remove
 #test test_next_move_bottom
     PointList* snake = create_cell(0, 1);
     Board* board = create_board(snake, NULL, 2, 2);
@@ -75,6 +78,7 @@
     fail_unless(next_move(board, UP) != NULL);
     fail_unless(next_move(board, RIGHT) != NULL);
 
+// Remove
 #test test_next_move_left
     PointList* snake = create_cell(1, 2);
     Board* board = create_board(snake, NULL, 4, 4);
@@ -82,6 +86,7 @@
     fail_unless(moved->x == 0);
     fail_unless(moved->y == 2);
 
+// Remove
 #test test_next_move_up
     PointList* snake = create_cell(2, 2);
     Board* board = create_board(snake, NULL, 4, 4);
@@ -89,6 +94,7 @@
     fail_unless(moved->x == 2);
     fail_unless(moved->y == 1);
 
+// Remove
 #test test_create_random_cell
     PointList* cell1 = create_random_cell(20, 10);
     PointList* cell2 = create_random_cell(20, 10);
@@ -98,12 +104,14 @@
     fail_unless(cell1->y < 10);
     fail_unless(cell2->y < 10);
 
+// Remove
 #test test_list_contains_true
     PointList* cell = create_cell(1, 2);
     PointList* list = create_cell(2, 2);
     list->next = create_cell(1, 2);
     fail_unless(list_contains(cell, list));
 
+// Remove
 #test test_list_contains_false
     PointList* cell = create_cell(4, 2);
     PointList* list = create_cell(2, 2);
@@ -142,14 +150,14 @@
 
     fail_unless(board->foods->x != 2 || board->foods->y != 2);
 
-
+// Remove
 #test test_remove_from_list_true
     PointList* cell = create_cell(1, 2);
     PointList* list = create_cell(2, 2);
     list->next = create_cell(1, 2);
     fail_unless(remove_from_list(cell, &list));
 
-
+// Remove
 #test test_remove_from_list_false
     PointList* cell = create_cell(3, 2);
     PointList* list = create_cell(2, 2);
