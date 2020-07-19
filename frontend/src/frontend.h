a34e607dbcdb29376b638cd679f823a63c61688e
Bartłomiej Osiński <bartlomiej.osinski@enigma.com.pl>
2020-07-19 22:30:28 +0200

    Ported frontend library (does not compile, requires ported main)

--- frontend/src/frontend.h
+++ frontend/src/frontend.h
@@ -1,3 +1,3 @@
 #include <ncurses.h>
-enum Direction get_next_move(enum Direction previous);
-void display_points(PointList* snake, const chtype symbol);
+extern enum Direction get_next_move(enum Direction previous);
+extern void display_points(PointList* snake, const chtype symbol);
--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,3 +1,42 @@
+use libc::c_int;
+use std::ptr;
+use ncurses::*;
+
+#[repr(C)]
+pub enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
+
+#[repr(C)]
+pub struct PointList {
+    x: c_int,
+    y: c_int,
+    next: *mut PointList,
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn get_next_move(previous: Direction) -> Direction {
+    let ch = getch();
+    match ch {
+        KEY_LEFT => { if let Direction::RIGHT = previous { previous } else { Direction::LEFT } }
+        KEY_RIGHT => { if let Direction::LEFT = previous { previous } else { Direction::RIGHT } }
+        KEY_DOWN => { if let Direction::UP = previous { previous } else { Direction::DOWN } }
+        KEY_UP => { if let Direction::DOWN = previous { previous } else { Direction::UP } }
+        _ => previous
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn display_points(mut snake: *mut PointList, symbol: chtype) {
+    while snake != ptr::null_mut() {
+        mvaddch((*snake).y as i32, (*snake).x as i32, symbol);
+        snake = (*snake).next;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -5,3 +44,4 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
